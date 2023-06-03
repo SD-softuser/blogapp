605c50aa462d8f072df02f031a9ed859f9eb293f
SD-softuser <sayogdmahajan@gmail.com>
2023-06-04 00:10:27 +0530

    blog page updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <main>
+    <header>
+      <a href="" class = "logo">MyBlog</a>
+      <nav> 
+        <a href="">Login</a>
+        <a href="">Register</a>
+      </nav>
+    </header>
+    <div class="post">
+      <div class='image'>
+        <img src = "https://techcrunch.com/wp-content/uploads/2023/06/apple-ar-vr.jpg?w=1390&crop=1"></img>
+      </div>
+      <div class="texts">
+        <h2>No one has done AR or VR well. Can Apple?</h2>
+        <p>On Monday, Apple is more than likely going to reveal its long-awaited augmented or mixed reality Reality Pro headset during the keynote of its annual WWDC developer conference in California.</p>
+      </div>
     </div>
+    <div class="post">
+      <div class='image'>
+        <img src = "https://techcrunch.com/wp-content/uploads/2023/06/apple-ar-vr.jpg?w=1390&crop=1"></img>
+      </div>
+      <div class="texts">
+        <h2>No one has done AR or VR well. Can Apple?</h2>
+        <p>On Monday, Apple is more than likely going to reveal its long-awaited augmented or mixed reality Reality Pro headset during the keynote of its annual WWDC developer conference in California.</p>
+      </div>
+    </div>
+    <div class="post">
+      <div class='image'>
+        <img src = "https://techcrunch.com/wp-content/uploads/2023/06/apple-ar-vr.jpg?w=1390&crop=1"></img>
+      </div>
+      <div class="texts">
+        <h2>No one has done AR or VR well. Can Apple?</h2>
+        <p>On Monday, Apple is more than likely going to reveal its long-awaited augmented or mixed reality Reality Pro headset during the keynote of its annual WWDC developer conference in California.</p>
+      </div>
+    </div>
+   </main>
   );
 }
 
