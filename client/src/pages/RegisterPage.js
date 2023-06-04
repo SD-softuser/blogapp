e4a62c4b7e8f1a11dc03176b014b9befcff379bd
SD-softuser <sayogdmahajan@gmail.com>
2023-06-04 12:09:28 +0530

    login register page added

--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -1,10 +1,10 @@
-export default function LoginPage(){ 
-    return (
-        <form class ="login">
-            <h1>Login</h1>
+export default function RegisterPage(){ 
+    return(
+        <form class="register">
+            <h1>Register</h1>
             <input type="text" placeholder="username"></input>
             <input type="password" placeholder="password"></input>
-            <button>Login</button>
+            <button>Register</button>
         </form>
     );
 }
